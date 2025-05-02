52978c436c6cf9c443d021695515feda2608d2e5
lucacasamentigamedev <luca.casamenti.gamedev@gmail.com>
2025-05-02 18:49:49 +0200

    star wars pull push mechanic

--- Source/UnrealPrototypes/Public/ForcePullPush/ForcePullPush.h
+++ Source/UnrealPrototypes/Public/ForcePullPush/ForcePullPush.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Camera/CameraComponent.h"
 #include "ForcePullPush.generated.h"
 
 
@@ -10,18 +11,29 @@ class UNREALPROTOTYPES_API UForcePullPush : public UActorComponent
 {
 	GENERATED_BODY()
 
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	AActor* TargetActor; //actor to pull/push
+	AActor* TraceTarget(float range, float radius, ETraceTypeQuery traceChannel, UCameraComponent* cameraComponent);
+
 public:	
 	// Sets default values for this component's properties
 	UForcePullPush();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UFUNCTION(BlueprintCallable)
-	AActor* FindTarget(float range, float radius, ETraceTypeQuery traceChannel);
+	void FindTarget(float range, float radius, ETraceTypeQuery traceChannel, UCameraComponent* cameraComponent);
+
+	UFUNCTION(BlueprintCallable)
+	void PullTarget(float speed, float deltaTime);
+
+	UFUNCTION(BlueprintCallable)
+	void StopPulling();
+
+	UFUNCTION(BlueprintCallable)
+	void PushTarget(float range, float radius, ETraceTypeQuery traceChannel, UCameraComponent* cameraComponent, float pushForce);
+
 };
